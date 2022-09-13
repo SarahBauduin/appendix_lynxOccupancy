75ac078020f68fe181420e1a05e46a81c70c9bc7
Sarah Bauduin <sarahbauduin@hotmail.fr>
2022-09-13 17:17:33 +0200

    Update occupancyModel.R

--- occupancyModel.R
+++ occupancyModel.R
@@ -26,37 +26,58 @@ load("data/gridFrComplete.RData")
 gridFrComplete <- crop(gridFrComplete, extent(c(720000, 1090000, 6260000, 6920000)))
 
 # Lynx presence data
-dataSCALP <- read.csv("C:/Users/sarah.bauduin/Documents/SaveOFB/Projets/8.SCALP/a classifier/2019-2020/codesSCALP.csv",
-                      header = TRUE, sep = ";")
-dataSCALP <- dataSCALP[!is.na(dataSCALP$X),]
-dataSCALP <- dataSCALP[!is.na(dataSCALP$Y),]
+lynxData1 <- read.csv("data/lynx/lynx_INDallR.csv", header = TRUE, sep = ";")
+lynxData2 <- read.csv("data/lynx/Export_Requete_pour_export.csv", header = TRUE, sep = ";")
+lynxData2 <- lynxData2[lynxData2$Fiabilité == "Retenu", ] # confirmed
+lynxData3 <- read.csv("data/lynx/Lynx_DOMLNE_1989-2018.csv", header = TRUE, sep = ";")
+lynxData4 <- read.csv("data/lynx/Lynx_DOMLNE_2019-2020.csv", header = TRUE, sep = ";")
+
+allLynxData <- cbind.data.frame(date = c(as.Date(lynxData1$Date.observation), 
+                                         format(as.Date(lynxData2$Date, format = "%d/%m/%y"), "%Y-%m-%d"), 
+                                         as.Date(lynxData3$DateAttaque), 
+                                         as.Date(lynxData4$Date.attaque)),
+                                X = c(lynxData1$X, lynxData2$x_l93, lynxData3$X, lynxData4$X),
+                                Y = c(lynxData1$Y, lynxData2$y_l93, lynxData3$Y, lynxData4$Y))
+
+allLynxData <- allLynxData[!is.na(allLynxData$date),]
+allLynxData <- allLynxData[!is.na(allLynxData$X),]
+allLynxData <- allLynxData[!is.na(allLynxData$Y),]
+
 # Add the coordinates system
-lynxData_spdf <- SpatialPointsDataFrame(coords = dataSCALP[,c("X", "Y")], data = dataSCALP, 
+lynxData_spdf <- SpatialPointsDataFrame(coords = allLynxData[,c("X", "Y")], data = allLynxData, 
                                          proj4string = CRS(as.character("+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")),
                                          match.ID = TRUE, bbox = NULL)
-lynxData_spdf$DateObservation <- as.Date(lynxData_spdf$DateObservation)
+lynxData_spdf$date <- as.Date(allLynxData$date, format = "%Y-%m-%d")
 lynxData_spdfTr <- spTransform(lynxData_spdf, gridFrComplete@proj4string)
 
 # Which cells are occupied during which year
-yearStart <- min(unique(lynxData_spdfTr$Annee))
-yearEnd <- max(unique(lynxData_spdfTr$Annee)) - 1
+yearStart <- as.numeric(format(min(lynxData_spdfTr$date), "%Y"))
+yearEnd <- as.numeric(format(max(lynxData_spdfTr$date), "%Y")) - 1
 obsLynx <- array(data = 0, dim = c(length(gridFrComplete$ID), 6, yearEnd - yearStart + 1))
 
 for(year in yearStart:yearEnd){
   
   # Extract the lynx observations for the different time period
-  novYear <- lynxData_spdfTr[lynxData_spdfTr$DateObservation >= as.Date(paste(year, "11", "01", sep = "-")) &
-                               lynxData_spdfTr$DateObservation < (as.Date(paste(year, "11", "01", sep = "-")))%m+% months(1),]
-  decYear <- lynxData_spdfTr[lynxData_spdfTr$DateObservation >= as.Date(paste(year, "12", "01", sep = "-")) &
-                               lynxData_spdfTr$DateObservation < (as.Date(paste(year, "12", "01", sep = "-")))%m+% months(1),]
-  janYearPlus1 <- lynxData_spdfTr[lynxData_spdfTr$DateObservation >= as.Date(paste(year + 1, "01", "01", sep = "-")) &
-                                    lynxData_spdfTr$DateObservation < (as.Date(paste(year + 1, "01", "01", sep = "-")))%m+% months(1),]
-  febYearPlus1 <- lynxData_spdfTr[lynxData_spdfTr$DateObservation >= as.Date(paste(year + 1, "02", "01", sep = "-")) &
-                                    lynxData_spdfTr$DateObservation < (as.Date(paste(year + 1, "02", "01", sep = "-")))%m+% months(1),]
-  marYearPlus1 <- lynxData_spdfTr[lynxData_spdfTr$DateObservation >= as.Date(paste(year + 1, "03", "01", sep = "-")) &
-                                    lynxData_spdfTr$DateObservation < (as.Date(paste(year + 1, "03", "01", sep = "-")))%m+% months(1),]
-  aprYearPlus1 <- lynxData_spdfTr[lynxData_spdfTr$DateObservation >= as.Date(paste(year + 1, "04", "01", sep = "-")) &
-                                    lynxData_spdfTr$DateObservation < (as.Date(paste(year + 1, "04", "01", sep = "-")))%m+% months(1),]
+  novYear <- lynxData_spdfTr[lynxData_spdfTr$date >= as.Date(paste(year, "11", "01", sep = "-")) &
+                               lynxData_spdfTr$date < (as.Date(paste(year, "11", "01", sep = "-")))%m+% months(1),]
+  decYear <- lynxData_spdfTr[lynxData_spdfTr$date >= as.Date(paste(year, "12", "01", sep = "-")) &
+                               lynxData_spdfTr$date < (as.Date(paste(year, "12", "01", sep = "-")))%m+% months(1),]
+  janYearPlus1 <- lynxData_spdfTr[lynxData_spdfTr$date >= as.Date(paste(year + 1, "01", "01", sep = "-")) &
+                                    lynxData_spdfTr$date < (as.Date(paste(year + 1, "01", "01", sep = "-")))%m+% months(1),]
+  febYearPlus1 <- lynxData_spdfTr[lynxData_spdfTr$date >= as.Date(paste(year + 1, "02", "01", sep = "-")) &
+                                    lynxData_spdfTr$date < (as.Date(paste(year + 1, "02", "01", sep = "-")))%m+% months(1),]
+  marYearPlus1 <- lynxData_spdfTr[lynxData_spdfTr$date >= as.Date(paste(year + 1, "03", "01", sep = "-")) &
+                                    lynxData_spdfTr$date < (as.Date(paste(year + 1, "03", "01", sep = "-")))%m+% months(1),]
+  aprYearPlus1 <- lynxData_spdfTr[lynxData_spdfTr$date >= as.Date(paste(year + 1, "04", "01", sep = "-")) &
+                                    lynxData_spdfTr$date < (as.Date(paste(year + 1, "04", "01", sep = "-")))%m+% months(1),]
+  
+  plot(gridFrComplete, main = paste0("Winter ", year, "-", year + 1))
+  plot(novYear, add = TRUE, col = "red", pch = 16)
+  plot(decYear, add = TRUE, col = "blue", pch = 16)
+  plot(janYearPlus1, add = TRUE, col = "green", pch = 16)
+  plot(febYearPlus1, add = TRUE, col = "yellow", pch = 16)
+  plot(marYearPlus1, add = TRUE, col = "orange", pch = 16)
+  plot(aprYearPlus1, add = TRUE, col = "purple", pch = 16)
   
   # Which cells were occupied
   if(length(novYear) != 0){
@@ -97,6 +118,12 @@ for(year in yearStart:yearEnd){
   }
 }
 
+# We only have the effort from winter 1993-1994 to winter 2020-2021
+# Lynx data are only valid until winter 2019-2020
+# We will restrict the effort data and lynx data from 1993-1994 to 2019-2020
+obsLynx <- obsLynx[,,(1993 - yearStart + 1):(1993 - yearStart + 1 + 2019 - 1993)]
+dim(obsLynx) # 2131 (n cells in the grid) x 6 months x 1993 to 2019
+effort <- effort[,(1993 - 1993 + 1):(1993 - 1993 + 1 + 2019 - 1993)]
 
 # Identify cells which were never sampled
 # Transform the effort in a binary variable
@@ -110,7 +137,6 @@ for(i in 1:nrow(samplEffort)){
     } 
   }
 }
-
 
 # Change format of obsLynx
 obsLynx2 <- obsLynx[, , 1]
